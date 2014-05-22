fc1de715aab9fa8960de27f24a521292f1599326
Matti Tahvonen <matti@vaadin.com>
2014-05-22 13:26:53 +0300

    removed widget set from source control (generated during build any was)

--- src/main/webapp/VAADIN/widgetsets/com.example.addressbook.AppWidgetSet/deferredjs/C521BC302CA06AC6AD975487B342CB3F/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_example_addressbook_AppWidgetSet.runAsyncCallback1('f8(1005,1,Umd);_.nc=function UZb(){iPb((!dPb&&(dPb=new jPb),dPb),this.b.e)};dbd(Vh)(1);\n//# sourceURL=com.example.addressbook.AppWidgetSet-1.js\n')
